59db485aaaba974d0a2b00d51c71bce405deb759
Levi Wilkerson <chelleas123@gmail.com>
2021-10-01 22:01:03 -0400

    added calorieprint and id tagging

--- js/app.js
+++ js/app.js
@@ -8,22 +8,23 @@
 // Add happy animation for previous day's calorie defecit
 //  dark mode?
 
+const CALORIECOUNTS_KEY = "calorieItems";
+const CALORIEITEM_BUTTON_CLASS = "calorie-item_btn";
+const CALORIEITEM_CLASS = "calorie-item";
 const calorieForm = document.querySelector(".calorie-form");
 const calorieInput = calorieForm.querySelector("input");
-
-const CALORIECOUNTS_KEY = "calorieItems";
 
 // Adds calorieCount array to local storage if  not present
 if (localStorage.getItem(CALORIECOUNTS_KEY) === null) {
   localStorage.setItem(CALORIECOUNTS_KEY, "[]");
 }
 
-const calories = localStorage.getItem(CALORIECOUNTS_KEY);
-
 function createCalorieObj(calories) {
+  const id = Math.random().toString(16).slice(2);
   return {
     date: new Date().getDay(),
     calories: calories !== undefined ? `${calories}` : "",
+    id: id,
   };
 }
 
@@ -36,10 +37,32 @@ function setCalories(calories) {
   localStorage.setItem(CALORIECOUNTS_KEY, newStringifiedItems);
 }
 
+function printAllCalories() {
+  const calorieItemsList = JSON.parse(localStorage.getItem(CALORIECOUNTS_KEY));
+  const ul = document.querySelector(".calorie-entries");
+  ul.replaceChildren();
+  calorieItemsList.map((item) => {
+    // creates lis
+    const li = document.createElement("li");
+    let nodeText = document.createTextNode(`${item.calories}`);
+    li.appendChild(nodeText);
+    li.id = item.id;
+    li.classList.add(CALORIEITEM_CLASS);
+    ul.appendChild(li);
+    // creates del buttons
+    const btn = document.createElement("button");
+    btn.id = item.id;
+    btn.classList.add(CALORIEITEM_BUTTON_CLASS);
+    li.appendChild(btn);
+  });
+}
+
 function onSubmit(event) {
   event.preventDefault();
   const input = calorieInput.value;
   setCalories(input);
+  printAllCalories();
 }
 
+printAllCalories();
 calorieForm.addEventListener("submit", onSubmit);
